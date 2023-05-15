46a853731e7a31e8980214b6fd9fc56772127152
Mohammad Mubassir <46520389+m-triple-m@users.noreply.github.com>
2023-05-15 11:52:40 +0530

    ..

---
+++ backend/models/jobModel.js
@@ -0,0 +1,18 @@
+const { Schema, model, Types } = require("../connection");
+
+const schema = new Schema({
+  brand: { type: Types.ObjectId, ref: "brand" },
+  title: { type: String, required: true },
+  incentive: { type: Number, required: true },
+  description: { type: Object },
+  type: { type: String },
+  data: { type: Object },
+  startDate: { type: Date },
+  endDate: { type: Date },
+  status: { type: String, default: "active" },
+  enrolled : [{ type: Types.ObjectId, ref: "user" }],
+  created_at: Date,
+  updated_at: Date,
+});
+
+module.exports = model("jobs", schema);
