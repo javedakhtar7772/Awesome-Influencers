0210c22e4689cdc8892bbe2e89432d027ebb0e5c
javedakhtar7772 <javedakhtar7772@gmail.com>
2023-05-19 13:07:44 +0530

    ...

--- frontend/src/components/main/Login.js
+++ frontend/src/components/main/Login.js
@@ -162,7 +162,7 @@ const Login = () => {
         <div className="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
           {/* Copyright */}
           <div className="text-white mb-3 mb-md-0">
-            Copyright © 2020. All rights reserved.
+          Copyright © 2023 Awesome Influensers . All Rights Reserved.
           </div>
           {/* Copyright */}
           {/* Right */}
