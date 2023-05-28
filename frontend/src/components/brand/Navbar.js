33228958ad3683cdfa4b72ad00570609810ff7f8
m-triple-m <triplem656@gmail.com>
2023-05-28 11:09:30 +0530

    ..

--- frontend/src/components/brand/Navbar.js
+++ frontend/src/components/brand/Navbar.js
@@ -1,10 +1,9 @@
-import React, { useState } from "react";
-import { Link, NavLink } from "react-router-dom";
-import app_config from "../../config";
-import { useBrandContext } from "../../context/BrandProvider";
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import app_config from '../../config';
+import { useBrandContext } from '../../context/BrandProvider';
 
 const Navbar = () => {
-
   const url = app_config.apiUrl;
 
   const { loggedIn, setLoggedIn, logout } = useBrandContext();
@@ -41,9 +40,9 @@ const Navbar = () => {
               </NavLink>
             </li>
             <li>
-            <NavLink className="dropdown-item" to="/brand/managejobs">
-              My Campaigns
-            </NavLink>
+              <NavLink className="dropdown-item" to="/brand/managejobs">
+                My Campaigns
+              </NavLink>
             </li>
             <li>
               <hr className="dropdown-divider" />
@@ -82,25 +81,16 @@ const Navbar = () => {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               {/* Navbar brand */}
               <a className="navbar-brand mt-2 mt-lg-0" href="#">
-                <img
-                  src="/Images/social-media.png"
-                  height={30}
-                  width={30}
-                  alt="Logo"
-                  loading="lazy"
-                />
+                <img src="/Images/social-media.png" height={30} width={30} alt="Logo" loading="lazy" />
               </a>
               {/* Left links */}
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item my-auto">
-                  <h3 className="m-0">
-                    BRAND DASHBORD
-                  </h3>
+                  <h3 className="m-0">BRAND DASHBORD</h3>
                 </li>
-                
               </ul>
               <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
-              <li className="nav-item">
+                <li className="nav-item">
                   <NavLink className="nav-link" to="/home">
                     Home
                   </NavLink>
@@ -125,7 +115,6 @@ const Navbar = () => {
                     Brand Login
                   </NavLink>
                 </li>
-                
               </ul>
               {/* Left links */}
             </div>
@@ -140,10 +129,5 @@ const Navbar = () => {
     </>
   );
 };
-
-
-
-
-
 
 export default Navbar;
